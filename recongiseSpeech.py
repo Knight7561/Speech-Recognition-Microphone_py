2fe54ce99cdf5cee547b80285d01d12b947407b5
Rohith Akash <37873469+Knight7561@users.noreply.github.com>
2019-10-30 19:05:14 +0530

    Update recongiseSpeech.py
    Corrections made with print statements

--- recongiseSpeech.py
+++ recongiseSpeech.py
@@ -1,9 +1,12 @@
+  
 import speech_recognition as sr
 
 r = sr.Recognizer()
 with sr.Microphone() as source:
-    print("Speak anything ti recongize ..  ")
+    print("Speak anything to recongize ..  ")
+    print("Listening...")
     audio = r.listen(source)
+    print("Audio Processing...")
     try:
         outpuText = r.recognize_google(audio)
         print("You said : ", format(outpuText))
